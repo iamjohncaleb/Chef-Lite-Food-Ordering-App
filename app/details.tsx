88298a4150911efcaa4490e395dbb4a12daa9da2
Caleb John <johncaleb022@gmail.com>
2024-04-19 03:15:18 -0700

    Update details.tsx

--- app/details.tsx
+++ app/details.tsx
@@ -11,8 +11,6 @@
 
 
 
-
-
 import { View, Text, StyleSheet, Image, TouchableOpacity, SectionList, ListRenderItem, ScrollView } from 'react-native';
 import React, { useLayoutEffect, useRef, useState } from 'react';
 import ParallaxScrollView from '@/Components/ParallaxScrollView';
